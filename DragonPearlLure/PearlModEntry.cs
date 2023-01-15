9bfd21d1d9bbeabb56e1ee2000abdd760670f147
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2023-01-15 17:51:26 -0500

    Initial commit, item exists but does not function

---
+++ DragonPearlLure/PearlModEntry.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace DragonPearlLure
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /// <summary>The Json Assets mod API.</summary>
+        private IJsonAssets JA_API;
+
+        /// <summary>The item ID for the pearl lure.</summary>
+        public int PearlLureID => this.JA_API.GetObjectId("violetlizabet.PearlLure");
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+        }
+
+        //
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // Grab JA API in order to create ring
+            JA_API = this.Helper.ModRegistry.GetApi<IJsonAssets>("spacechase0.JsonAssets");
+            if (JA_API == null)
+            {
+                this.Monitor.Log("Could not get Json Assets API, mod will not work!", LogLevel.Error);
+            }
+            else
+            {
+                this.JA_API.LoadAssets(Path.Combine(this.Helper.DirectoryPath, "assets", "json-assets"), this.Helper.Translation);
+            }
+        }
+
+        // Get the Json Assets ID
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Generate explosion animation when placed
+        private bool DoPearlExplosionAnimation(GameLocation location, int x, int y, Farmer who)
+        {
+            Vector2 placementTile = new Vector2(x / 64, y / 64);
+            var Game1_multiplayer = this.Helper.Reflection.GetField<Multiplayer>(typeof(Game1), "multiplayer").GetValue();
+            foreach (TemporaryAnimatedSprite temporarySprite2 in location.temporarySprites)
+            {
+                if (temporarySprite2.position.Equals(placementTile * 64f))
+                {
+                    return false;
+                }
+            }
+            int idNum = Game1.random.Next();
+            location.playSound("thudStep");
+            Game1_multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite(PearlLureID, 100f, 1, 24, placementTile * 64f, flicker: true, flipped: false, location, who)
+            {
+                shakeIntensity = 0.5f,
+                shakeIntensityChange = 0.002f,
+                extraInfoForEndBehavior = idNum,
+                endFunction = location.removeTemporarySpritesWithID
+            });
+            Game1_multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(598, 1279, 3, 4), 53f, 5, 9, placementTile * 64f, flicker: true, flipped: false, (float)(y + 7) / 10000f, 0f, Color.Yellow, 4f, 0f, 0f, 0f)
+            {
+                id = idNum
+            });
+            Game1_multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(598, 1279, 3, 4), 53f, 5, 9, placementTile * 64f, flicker: true, flipped: false, (float)(y + 7) / 10000f, 0f, Color.Orange, 4f, 0f, 0f, 0f)
+            {
+                delayBeforeAnimationStart = 100,
+                id = idNum
+            });
+            Game1_multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite("LooseSprites\\Cursors", new Microsoft.Xna.Framework.Rectangle(598, 1279, 3, 4), 53f, 5, 9, placementTile * 64f, flicker: true, flipped: false, (float)(y + 7) / 10000f, 0f, Color.White, 3f, 0f, 0f, 0f)
+            {
+                delayBeforeAnimationStart = 200,
+                id = idNum
+            });
+            location.netAudio.StartPlaying("fuse");
+            return true;
+        }
+    }
+}
